e99c3af590da0473fbd21c3209424919f1b530de
Daniel Crisp <crispd@star.lcc.edu>
2016-11-03 20:04:35 -0400

    Added in class project for MultiDimensional array, and a copy of my array IO to projects

---
+++ weeks/WK10/MultiDimArray.cpp
@@ -0,0 +1,48 @@
+// Author:		DAN CRISP
+// DATE:		3NOV2016
+// FILE:		MultiDimArray
+/*	DESC	*/
+
+#include<iostream>
+#include<cstdlib>
+#include<cmath>
+
+int main()
+{
+	using namespace std;
+	int t, i, num[3][4], nums[3][4];
+	system("cls");
+	
+	int count = 1;
+	int idim = 4;
+	int tdim = 3;
+	
+	cout << "\nWe're making a multidimensional array.\n";
+	for (t=0; t<3; ++t) {
+		for (i=0; i<4; ++i) {
+			num[t][i] = count;
+			cout << num[t][i] << "\t";
+			count++;
+		}
+		cout << "\n";
+	}
+	
+	cout << "\n\n";
+	
+	for (t=0; t<tdim; ++t) {
+		for (i=0; i<idim; ++i) {
+			nums[t][i] = (idim-i)+4*tdim;
+			cout << num[t][i] << "\t";
+		}
+		cout << "\n";
+	}
+	
+	
+	
+
+	for (int x : num ) {
+		cout << x << "\t";
+	}
+	
+	return 0;
+}
