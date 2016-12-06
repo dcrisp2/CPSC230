448f43c0f14b189f9d621e3cc4c1c2b091b14ff9
crispd <crispd@DESKTOP-FFQN2HH>
2016-12-06 13:44:37 -0500

    started scaling code

---
+++ projects/parsing/scaling.cpp
@@ -0,0 +1,33 @@
+//AUTHOR:		DANIEL CRISP
+//DATE:			Tuesday, Dec 6th, 2016
+//DESC:			scaling.cpp
+/*
+
+*/
+
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+
+int main ()
+{
+	const double e = ;
+	const double Rho = ;
+	int Q;
+	double B, A;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
